4dee325e11f23d44ca223d6b8877b51ee8c0b089
Edivilson Dalacosta <edivilsondalacosta@gmail.com>
2022-12-12 18:09:58 -0300

    refactor product validation

--- clean-arch/src/domain/product/entity/product.ts
+++ clean-arch/src/domain/product/entity/product.ts
@@ -1,6 +1,5 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
-import ProductValidatorFactory from "../factory/product.validator.factory";
 
 export default class Product extends Entity {
   private _name: string;
@@ -36,6 +35,23 @@ export default class Product extends Entity {
   }
 
   validate() {
-    ProductValidatorFactory.create().validate(this);
+    if (this.id.length === 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Id is required",
+      });
+    }
+    if (this.name.length === 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Name is required",
+      });
+    }
+    if (this.price <= 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Price must be greater than zero",
+      });
+    }
   }
 }
